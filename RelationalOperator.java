94273a1a4177cd9d113262b3f7ce9ed0c60b86d3
Swapnika Mohanty <swapnikamohanty@gmail.com>
2025-01-28 21:38:41 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st number");
+        int a = sc.nextInt();
+        System.out.println("enter 2nd number");
+        int b = sc.nextInt();
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        sc.close();
+    }
+}
